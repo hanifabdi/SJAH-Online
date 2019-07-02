d6c141877644f4064888cc514a9d9c9649b435b4
Hanif Abdillah <hanifabdillah61@gmail.com>
2019-07-02 13:46:04 +0700

    Initial commit

---
+++ application/views/Pengguna/v_beranda.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- My Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans|Viga&display=swap" rel="stylesheet">
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/vendor/bootstrap/style.css">
+
+
+    <title>Hanif_Official</title>
+  </head>
+  <body>
+     <div class="container base">
+  <div class="container">
+      <div class="row " style=" box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.5); padding: 1px;"  >
+        <div class="col-md-3">
+         <a href="<?php base_url() ?>">
+           <img src="<?php base_url() ?>assets/image/logo.png" width="260px" height="70px" style="margin-top:25px;">
+         </a>
+        </div>
+        <div class="col-md-3">
+        </div>
+      </div>
+  </div>
+
+     <?php $this->load->view('header');?>
+
+     <marquee class="textjalan"> Selamat Datang di SJA-H Online  ||  SJA-H Online adalah sebuah Sistem Informasi Statistik Jasa Akomodasi Hotel berbasis Web  </marquee>
+     <!-- 
+         
+    <div class="jumbotron text-center">
+        <div class="container">
+        <img class="foto" src="assets/image/Hanif.jpg" class="rounded-circle"> 
+        <h1 class="namaprofil"> Hanif Abdillah </h1>
+        <p class="prodi"> Mahasiswa Informatika </p>
+        </div>
+    </div>
+    -->
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light ">
+  <div class="container">
+    <a class="navbar-brand" href="<?php echo base_url(); ?>">
+    <img src="<?php echo base_url('assets/image/logo.png')?>">
+  </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="<?php echo base_url();?>">BERANDA <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ENTRI DATA</a>
+        </li>
+       
+         <li class="nav-item">
+          <a class="nav-link" href="#">CONTACT</a>
+        </li>
+        </ul>
+    </div>
+  </div>
+</nav>
